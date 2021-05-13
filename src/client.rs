7d429b195df4d286903686bf00caa1c11dc380e4
Nikita Sivakov <sivakov512@gmail.com>
2021-05-14 01:01:51 +0300

    Support

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,6 @@
+use futures_util::stream::StreamExt;
 use lapin::{
-    options::{BasicPublishOptions, QueueDeclareOptions},
+    options::{BasicConsumeOptions, BasicPublishOptions, QueueDeclareOptions},
     types::FieldTable,
     BasicProperties, Channel, Connection, ConnectionProperties,
 };
@@ -9,13 +10,38 @@ use tokio_amqp::*;
 pub struct RmqRpcClient {
     conn: Connection,
     channel: Channel,
+    fast_reply_task: tokio::task::JoinHandle<()>,
 }
 
 impl RmqRpcClient {
     pub async fn connect(url: &str) -> Result<Self, lapin::Error> {
         let conn = Connection::connect(url, ConnectionProperties::default().with_tokio()).await?;
         let channel = conn.create_channel().await?;
-        Ok(Self { conn, channel })
+
+        let cloned_channel = channel.clone();
+        let fast_reply_task = tokio::spawn(async move {
+            let mut consumer = cloned_channel
+                .basic_consume(
+                    "amq.rabbitmq.reply-to",
+                    "",
+                    BasicConsumeOptions {
+                        no_ack: true,
+                        ..BasicConsumeOptions::default()
+                    },
+                    FieldTable::default(),
+                )
+                .await
+                .unwrap();
+            while let Some(delivery) = consumer.next().await {
+                dbg!(delivery.unwrap());
+            }
+        });
+
+        Ok(Self {
+            conn,
+            channel,
+            fast_reply_task,
+        })
     }
 
     pub async fn declare_queue(&self, queue: &str) -> Result<&Self, lapin::Error> {
@@ -36,7 +62,7 @@ impl RmqRpcClient {
                 routing_key,
                 BasicPublishOptions::default(),
                 payload,
-                BasicProperties::default(),
+                BasicProperties::default().with_reply_to("amq.rabbitmq.reply-to".into()),
             )
             .await?;
         Ok(())
@@ -82,10 +108,13 @@ mod send_message {
         const QUEUE: &str = "message_sent";
         const DATA: &str = "Hello";
         let channel = create_channel().await;
-
-        RmqRpcClient::connect(URL)
+        channel
+            .queue_delete(QUEUE, lapin::options::QueueDeleteOptions::default())
             .await
-            .unwrap()
+            .unwrap();
+
+        let client = RmqRpcClient::connect(URL).await.unwrap();
+        client
             .declare_queue(QUEUE)
             .await
             .unwrap()
@@ -99,5 +128,40 @@ mod send_message {
             .unwrap()
             .unwrap();
         assert_eq!(String::from_utf8_lossy(&msg.delivery.data), DATA.to_owned())
+    }
+
+    #[tokio::test]
+    async fn message_has_reply_to_queue() {
+        const QUEUE: &str = "message_has_reply_to_queue";
+        const DATA: &str = "Hello";
+        let channel = create_channel().await;
+        channel
+            .queue_delete(QUEUE, lapin::options::QueueDeleteOptions::default())
+            .await
+            .unwrap();
+
+        let client = RmqRpcClient::connect(URL).await.unwrap();
+        client
+            .declare_queue(QUEUE)
+            .await
+            .unwrap()
+            .send_message(QUEUE, DATA.as_bytes().to_vec())
+            .await
+            .unwrap();
+
+        let msg = channel
+            .basic_get(QUEUE, lapin::options::BasicGetOptions::default())
+            .await
+            .unwrap()
+            .unwrap();
+        assert!(msg.delivery.properties.reply_to().is_some());
+        assert!(msg
+            .delivery
+            .properties
+            .reply_to()
+            .clone()
+            .unwrap()
+            .to_string()
+            .contains("amq.rabbitmq.reply-to"));
     }
 }
